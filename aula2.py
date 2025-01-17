ced42f032b01716bc3321b9d69b7b993b612a792
Lucas Yuji <160159812+LucaYuji@users.noreply.github.com>
2025-01-17 14:00:21 -0300

    Add files via upload

---
+++ aula2.py
@@ -0,0 +1,40 @@
+# Tipos primitivos de dados em python
+
+# Interger ou int, número inteiro
+print(10)
+# Float, reais ou ponto flutuante
+print(10.5)
+# String, texto
+print('isso é um texto')
+# Boleanos, Verdadeiro ou Falso
+print(True, False)
+
+
+
+# Operadores
+# + - * / ** % //
+
+# Operadores Lógicos
+# > < == >= <=
+
+
+nome = 'Pedro'
+idade = 25
+curso = 'Programção em Python'
+# concatenação == juntar variaveis
+print('Nome:', nome, 'Idade:', idade,'Curso:', curso)
+print(f'Nome: {nome} idade: {idade} curso: {curso}')
+print('Nome: {} Idade: {} Curso: {}'.format(nome, idade, curso))
+print('Nome: %s Idade: %s Curso: %s'%(nome, idade, curso))
+
+# print(200**2**6)
+# print(200**2)
+# print(2**6)
+# print(200**64)
+
+a = 6
+b = 3
+a /= 2 * b
+print(a)
+ 
+'''documentar uma função'''
